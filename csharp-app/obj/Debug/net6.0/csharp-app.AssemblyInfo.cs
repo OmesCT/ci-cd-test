69aa50c0cc0e115d36c806c2a4dfdb6805afd12a
Chinnawat.t <chinnawat.t@TCNB65559.local>
2026-05-26 14:09:47 +0700

    Fix: Move .github to root for GitHub Actions to run

--- csharp-app/obj/Debug/net6.0/csharp-app.AssemblyInfo.cs
+++ csharp-app/obj/Debug/net6.0/csharp-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec60602e1798da15aa8f17a47b4aadf010cdc15")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
